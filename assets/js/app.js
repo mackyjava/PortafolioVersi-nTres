f758c9cc07f19c1d8e7cd02849bf5f256d6c3356
maggie jv <maggie jv>
2017-07-30 21:47:10 -0500

    version 3 terminada maquetacion y funcionalidad con angular y jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,11 +40,4 @@
 
   });
 
-  // app.filter('imageify', function () {
-  //   return function (input) {
-  //     var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
-  //     return url;
-  //   };
-  // });
-
 })();
---
+++ assets/js/modal.js
@@ -0,0 +1,9 @@
+var cargarPagina = function(){
+  $('#myModal').modal('toggle');
+  $('#ModalDos').modal('toggle');
+  $('#ModalTres').modal('toggle');
+};
+
+
+
+$(document).ready(cargarPagina);
